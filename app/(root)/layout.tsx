6e36af0a6cace8322c892e78e9cefed284c26503
sGYAN21 <sgyan5879@gmail.com>
2025-09-23 01:24:13 +0530

    Call Status add

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -13,7 +13,7 @@ const RootLayout = async ({children} :{children: ReactNode}) => {
     <div className='root-layout'>
       <nav>
         <Link href="/" className='flex items-center gap-2'>
-        <Image src="/logo.svg" alt='Logo' width={38} height={32}/>
+        <Image src="/logo.svg" alt='Logo' width={38} height={38}/>
         <h2 className='text-primary-100'>MockTalk</h2>
         </Link>
       </nav>
